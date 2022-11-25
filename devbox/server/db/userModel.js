b140fe90e9efc813202fbb80d0b970d5448c915f
ts <tejfsingh@gmail.com>
2022-11-24 21:56:47 -0800

    Fixed git

---
+++ devbox/server/db/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+// user schema
+const UserSchema = new mongoose.Schema({
+  // email field
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+  username: {
+    type: String,
+    required: [true, "Please provide a Username!"],
+    unique: [true, "Username Exist"],
+  },
+  //   password field
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+
+// export UserSchema
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
