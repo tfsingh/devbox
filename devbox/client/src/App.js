e7b9b701e2c6376d9af8c2ea29cbae519638065e
ts <tejfsingh@gmail.com>
2023-01-04 17:58:39 -0800

    created client

---
+++ devbox/client/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input className="username"></input>
+
+        <input className="username"></input>
+        <button></button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
